a9c58f48a954be3faac42d80627219cfebda470d
squee72564 <rodriguez.al183@gmail.com>
2024-02-28 22:44:17 -0800

    updates

--- market_bot.js
+++ market_bot.js
@@ -1,9 +1,13 @@
 const axios = require('axios');
+const puppeteer = require('puppeteer-extra');
+const stealth_plugin = require('puppeteer-extra-plugin-stealth')
+
+puppeteer.use(stealth_plugin())
 
 // This function makes a POST request to get the JSON of all
 // items currently on the market. It returns a list with a JSON
 // object for each item
-const get_market_items_json = async () => {
+const get_market_items_json = async (market_items_data) => {
     return await axios.post(`https://voxiom.io/market/public`, {} ,{
         headers: {
             'User-Agent': '=)',
@@ -11,13 +15,26 @@ const get_market_items_json = async () => {
     })
     .then( (response) => {
         if (!response.data.success) throw new Error(`Reponse for market data not sucessfull.`);
+        market_items = response.data.data.market_items;
+    
+        for (let i = 0; i < market_items.length; i++) {
+            const additional_item_data = market_items_data.get(i); 
 
-        return response.data.data.market_items;
+            market_items[i].index = i;
+            market_items[i].skin_name = additional_item_data[0];
+            market_items[i].skin_rarity = additional_item_data[1];
+        }
+
+        return market_items;
     })
     .catch( (error) => {
         console.log("Error getting market items:");
         throw error;
     });
+}
+
+const attach_listing_metadata = (market_items) => {
+
 }
 
 // This function is used to attach the average price information to each JSON object
@@ -62,8 +79,6 @@ const get_average_price = async (item_id) => {
     .then( (response) => {
         if (!response.data.success) throw new Error(`Error with response for item.`);
         
-        //console.log(`Getting avg price info for ${JSON.stringify(response.data.data.item_info.seller_name)}`);
-
         const sale_history = response.data.data.item_info.price_history;
 
         if (sale_history.length === 0) throw new Error(`No previous history for item ${item_id} to average.`);
@@ -124,12 +139,22 @@ const consolidate_items_by_seller = (market_items) => {
     let market_items_by_seller = { };
 
     for (const item of market_items) {
-        const {seller_name, item_type, price, average_price, ...item_without_seller} = item;
+        const {
+            seller_name,
+            price,
+            average_price,
+            index,
+            skin_name,
+            skin_rarity,
+            ..._
+        } = item;
 
         const item_info = {
-            item_type: item_type,
+            skin_name: skin_name,
+            skin_rarity: skin_rarity,
             price: price,
             average_price: average_price,
+            index: index,
         };
 
         if (!(seller_name in market_items_by_seller)) {
@@ -143,8 +168,94 @@ const consolidate_items_by_seller = (market_items) => {
 }
 
 
-const run_bot = () => {
-    get_market_items_json()
+const check_if_next_page_available = async (page, svg_selector) => {
+
+    await page.waitForSelector(svg_selector);
+
+
+    // Wait until the svg for the next page button shows up
+    const svg_element = await page.$(svg_selector);
+
+    if (svg_element) {
+
+        const parent_div = await svg_element.evaluateHandle( svg => {
+            return svg.parentElement;
+        });
+
+        const style_attribute = await parent_div.evaluate( div => {
+            const computed_style = window.getComputedStyle(div);
+
+            return computed_style.getPropertyValue('visibility')
+        })
+
+        if (style_attribute === 'visible') {
+            return true;
+        } else {
+            return false;
+        }
+
+    } else {
+        return false;
+    }
+}
+
+const run_bot = async () => {
+
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms)); 
+    
+    let market_items_data = new Map();
+    
+    await puppeteer.launch({headless: true})
+    .then(async headless_browser => {
+        console.log(`Using headless browser to get data for market listings...`);
+        
+        const page = await headless_browser.newPage();
+        await page.goto('https://voxiom.io/loadouts/market', {waitUntil: ['load', 'domcontentloaded']});
+        
+        // Initial delay to let page load JS
+        await delay(5000);
+        
+        // Used to index the items to be matched later with pricing data
+        let index = 0;
+        
+        // selector for the next page button
+        const svg_selector = '.fa-angle-right';
+        let is_next_page_available = false;
+
+        do {
+
+            const items = await page.$$(
+                'div.bSIkxv'
+            );
+
+            for (const item of items) {
+                const childDivs = await item.$$('div');
+                
+                const childDivTextList = await Promise.all(childDivs.map(async (childDiv) => {
+                    return await childDiv.evaluate(div => div.innerText);
+                }));
+
+                market_items_data.set(index, childDivTextList.slice(-3));
+                
+                index++;
+            }
+
+            is_next_page_available = await check_if_next_page_available(page, svg_selector);
+            
+            if (is_next_page_available) {
+                await page.click(svg_selector);
+                await delay(1000);
+            }
+
+        } while (is_next_page_available);
+
+        console.log(`\tDone.`);
+        await headless_browser.close();
+    });
+
+    console.log(market_items_data);
+
+    await get_market_items_json(market_items_data)
         .then( (initial_items) => {
             console.log(`Initial market item count: ${initial_items.length}`);
             return attach_average_price_history(initial_items);
@@ -169,4 +280,4 @@ const run_bot = () => {
 }
 
 run_bot();
-setInterval(() => run_bot(), 1200000);
+setInterval(async () => await run_bot(), 180000);
